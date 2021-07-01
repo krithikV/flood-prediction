76f8eff668fd3425f056ce5e2f5155a59efe85f0
krithikV <45972552+krithikV@users.noreply.github.com>
2021-07-01 18:23:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from flooddetection import get
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html");   
+@app.route("/", methods = ['POST'])
+def final():
+    text = request.form['text']
+    labe = get(text)
+    return render_template('final.html', url = text, stat = labe)
+
+if __name__ == "__main__":
+    app.run('127.0.0.1',port='2523')
